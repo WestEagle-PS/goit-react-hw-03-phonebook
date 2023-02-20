15c756f40d0ef64634dac0eeb1aa0964cf6fd731
Anton Orlov <anton_orlov@ukr.net>
2023-02-20 21:54:34 +0200

    started files install

---
+++ src/components/PhoneBook/PhoneBlock/PhoneBlock.jsx
@@ -0,0 +1,17 @@
+import PropTypes from 'prop-types';
+import css from './phone-block.module.scss';
+
+const PhoneBlock = ({ title, children }) => {
+  return (
+    <div className={css.wrapper}>
+      <h3 className={css.title}>{title}</h3>
+      {children}
+    </div>
+  );
+};
+
+export default PhoneBlock;
+
+PhoneBlock.propTypes = {
+  title: PropTypes.string.isRequired,
+};
---
+++ src/components/PhoneBook/PhoneBook.jsx
@@ -0,0 +1,109 @@
+import { nanoid } from 'nanoid';
+import { Component } from 'react';
+import PropTypes from 'prop-types';
+import PhoneBlock from './PhoneBlock/PhoneBlock';
+import ContactList from './ContactList/ContactList';
+import ContactForm from './ContactForm/ContactForm';
+import css from './phone-book.module.scss';
+
+class PhoneBook extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+
+  onAddContacts = ({ name, number }) => {
+    if (this.isDublicate({ name, number })) {
+      return alert(`${name} ${number} is already in contacts`);
+    }
+
+    this.setState(prevState => {
+      const { contacts } = prevState;
+      const newPhone = {
+        id: nanoid(),
+        name,
+        number,
+      };
+
+      return { contacts: [...contacts, newPhone] };
+    });
+  };
+
+  onDeleteNumber = id => {
+    this.setState(prevState => {
+      const newContacts = prevState.contacts.filter(item => item.id !== id);
+      return {
+        contacts: newContacts,
+      };
+    });
+  };
+
+  isDublicate({ name, number }) {
+    const { contacts } = this.state;
+    const normalizedName = name.toLowerCase();
+    const dublicate = contacts.find(contact => {
+      return (
+        contact.name.toLowerCase() === normalizedName &&
+        contact.number === number
+      );
+    });
+
+    return Boolean(dublicate);
+  }
+
+  getFilteredNumbers() {
+    const { filter, contacts } = this.state;
+
+    // if (filter.length < 1) {
+    //   return contacts;
+    // }
+
+    const normalizedFilter = filter.toLowerCase();
+    const result = contacts.filter(({ name }) => {
+      return name.toLowerCase().includes(normalizedFilter);
+    });
+
+    return result;
+  }
+
+  handleFilterChange = e => {
+    const { value } = e.target;
+    this.setState({ filter: value });
+  };
+
+  render() {
+    const { onAddContacts, onDeleteNumber } = this;
+    const contacts = this.getFilteredNumbers();
+
+    return (
+      <div className={css.wrapper}>
+        <h2 className={css.title}>My Phonebook</h2>
+        <PhoneBlock title="Phonebook">
+          <ContactForm onSubmit={onAddContacts} />
+        </PhoneBlock>
+        <PhoneBlock title="Contacts">
+          <label className={css.label}>Find contacts by name:</label>
+          <input
+            onChange={this.handleFilterChange}
+            className={css.textField}
+            name="filter"
+            value={this.state.filter}
+          />
+          <ContactList contacts={contacts} onDeleteNumber={onDeleteNumber} />
+        </PhoneBlock>
+      </div>
+    );
+  }
+}
+
+export default PhoneBook;
+
+PhoneBook.propTypes = {
+  contacts: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      number: PropTypes.number.isRequired,
+    })
+  ),
+  filter: PropTypes.string,
+};
